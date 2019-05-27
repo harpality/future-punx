4b367d4fc179088b66f371dba29f8ab124e75f92
Speedway <harpalassi@gmail.com>
2019-05-27 13:11:35 -0500

    header component

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,39 @@ import { Link } from "gatsby"
 
 const Header = () => {
   return (
-    <nav
-      style={{
-        display: "flex",
-        listStyle: "none",
-        justifyContent: "space-between",
-        width: "20%",
-      }}
-    >
-      <li>
-        <Link to="/about">about</Link>
-      </li>
-      <li>
-        <Link to="/contact">contact</Link>
-      </li>
-      <li>
-        <Link to="/">home</Link>
-      </li>
-      <li>
-        <Link to="/tourdates">tour dates</Link>
-      </li>
-    </nav>
+    <header style={{ display: "flex", flexDirection: "column" }}>
+      <img
+        style={{
+          maxWidth: "700px",
+        }}
+        src="https://f4.bcbits.com/img/0015456712_100.png"
+        alt="futurepunx"
+      />
+      <nav>
+        <ul
+          style={{
+            display: "flex",
+            listStyle: "none",
+            justifyContent: "space-between",
+            width: "30%",
+            flexWrap: "wrap",
+          }}
+        >
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/contact">contact</Link>
+          </li>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/tourdates">tour dates</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
